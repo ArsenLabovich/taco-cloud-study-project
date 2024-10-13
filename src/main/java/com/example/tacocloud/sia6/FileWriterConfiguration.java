0047944829198ae36afbdbb3c3fa7a735570d5f2
ArsenLabovich <148917700+ArsenLabovich@users.noreply.github.com>
2024-10-13 14:11:59 +0200

    First Upload of study project

--- src/main/java/com/example/tacocloud/sia6/FileWriterConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.example.tacocloud.sia6;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource("classpath:/filewriter-config.xml")
-public class FileWriterConfiguration {
-}
